9dd52e0e4f8ec3bb46e590a7a746b7c13498baac
Dani <61524513+daniellanev4@users.noreply.github.com>
2021-10-27 11:51:37 -0400

    Update sandbox.js

--- AtomJS/sandbox.js
+++ AtomJS/sandbox.js
@@ -1,2 +1,42 @@
-console.log(1);
-console.log(2);
+// let age = 25;
+// let year = 2019;
+
+//log things to console
+// console.log(age, year);
+
+/* age = 30;
+console.log(age);
+
+const points = 100;
+points = 50;
+console.log(points); */
+
+// var score = 75;
+// console.log(score);
+
+// strings
+/* console.log('hello, world');
+
+let email = 'mario@thenetninja.co.uk';
+console.log(email);
+
+// string concatention
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+
+let fullName = firstName +  ' ' + lastName;
+console.log(fullName);
+
+//getting characters
+console.log(fullName[2]);
+
+//string length
+console.log(fullName.length);
+
+//string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index); */
